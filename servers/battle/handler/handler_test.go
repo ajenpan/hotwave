014ca364d74879b909976acf0c44d2d9c94cf512
ajen <ajenpan@gmail.com>
2022-03-26 10:26:13 +0800

    add bttle servers

---
+++ servers/battle/handler/handler_test.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"context"
+	"testing"
+
+	protobuf "google.golang.org/protobuf/proto"
+
+	"hotwave/servers/battle"
+	"hotwave/servers/battle/noop"
+	pb "hotwave/servers/battle/proto"
+	gatewayproto "hotwave/servers/gateway/proto"
+)
+
+func createTestHandler() *Handler {
+	h := New()
+	h.LogicCreator = &battle.GameLogicCreator{}
+	h.LogicCreator.Store("noop", noop.NewGameLogic)
+	return h
+}
+func TestCreateBattle(t *testing.T) {
+	h := createTestHandler()
+
+	ctx := context.Background()
+	in := &pb.CreateBattleRequest{
+		GameName: "noop",
+	}
+	// out := &pb.CreateBattleResponse{}
+	h.CreateBattle(ctx, in)
+}
+func newMessageWarp(msg protobuf.Message) *gatewayproto.UserMessageWraper {
+	ret := &gatewayproto.UserMessageWraper{
+		Gateway: "",
+		Name:    string(protobuf.MessageName(msg)),
+		UserId:  1,
+	}
+	ret.Body, _ = protobuf.Marshal(msg)
+	return ret
+}
+
+func TestOnUserMessage(t *testing.T) {
+	h := createTestHandler()
+
+	warper := newMessageWarp(&pb.CreateBattleRequest{})
+
+	h.OnUserMessage(warper)
+
+	// md := pb.File_servers_battle_proto_battle_proto.Messages().ByName(protoreflect.Name(warper.Name))
+	// msg := md.Options().ProtoReflect().New()
+	// msg:= pb.File_servers_battle_proto_battle_proto.Options().ProtoReflect().New()
+
+	// protobuf.Unmarshal(warper.Body, msg)
+
+	// mt,_:= preg.GlobalTypes.FindMessageByName(protoreflect.FullName(md.FullName()))
+	// mt.New()
+
+	// h.OnUserMessage()
+	// h.OnUserMessage()
+}
---
+++ servers/battle/player.go
@@ -0,0 +1,16 @@
+package battle
+
+import (
+	protobuf "google.golang.org/protobuf/proto"
+
+	"hotwave/user"
+)
+
+type Player interface {
+	user.User
+	GetSeatID() SeatID
+	GetScore() int64 //game jetton
+	IsRobot() bool
+
+	SendMessage(protobuf.Message) error
+}
